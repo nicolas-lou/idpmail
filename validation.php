20b32aebc3aca60738003ad4143dc6e97587adf6
nicolas-lou <git.nicolas-lou@laposte.net>
2018-11-15 13:38:01 +0100

    début css

--- validation.php
+++ validation.php
@@ -4,29 +4,15 @@
 
 $login = $_POST['login'];
 $password = $_POST['pass'];
-
 $resultats=$connexion->query("SELECT * from users WHERE login like $login and password like '$password'");
 $resultats->setFetchMode(PDO::FETCH_OBJ);
+$row =$resultats->fetchAll();
 
 
-if(isset($resultats)){
-     foreach($resultats as $row){
-          $_SESSION['prenom']=$row->prenom;
-          $_SESSION['nom']=$row->nom;   
-          $_SESSION['login']=$row->login;       
-        };
-        header("Location: /accueil/accueil.php");
-
-}else{
-     header("Location: /index.php");
-}
-
-
-
-
-
-
-
-
-
+$_SESSION['prenom']=$row[0]->prenom;
+$_SESSION['nom']=$row[0]->nom;   
+$_SESSION['login']=$row[0]->login;
+header("Location: ../accueil/accueil.php");
+          
+       
 ?>
