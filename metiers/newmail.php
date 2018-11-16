8c7eec6a57470fc169f83df3b36e2564e0dcedd4
nicolas-lou <git.nicolas-lou@laposte.net>
2018-11-16 08:43:23 +0100

    css v1

--- metiers/newmail.php
+++ metiers/newmail.php
@@ -15,7 +15,7 @@ if(is_null($_SESSION['prenom'])){
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>       
-        <link rel="stylesheet" type="text/css" media="screen" href="../css/index.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="../css/style.css" />
         <script src="../js/index.js"></script>
     </head>
     <body>
